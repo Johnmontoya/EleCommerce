7f56a117e52e1ce32de998ec59b702d9d95e40bd
John Montoya <johnmontoya777@hotmail.com>
2026-01-20 18:20:38 -0500

    eliminacion de interceptors, no se estan usando

--- client-ecommerce/src/shared/api/client.ts
+++ client-ecommerce/src/shared/api/client.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import token from "../utils/token";
 
 export const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -10,27 +9,3 @@ export const apiClient = axios.create({
     },
     timeout: 10000,
 });
-
-apiClient.interceptors.request.use(
-    (config) => {
-        const tokenData = token.getToken('token');
-        if (tokenData) {
-            config.headers.Authorization = `Bearer ${tokenData}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-)
-
-apiClient.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response?.status === 401) {
-            token.removeToken('token');
-            window.location.href = '/login';
-        }
-        return Promise.reject(error);
-    }
-)
