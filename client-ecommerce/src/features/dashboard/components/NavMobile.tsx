d3f01f9ae31f9208ce1fe5ad75b060fec29c6b2f
John Montoya <johnmontoya777@hotmail.com>
2026-03-04 23:44:27 -0500

    Navmobile modified to see all links

--- client-ecommerce/src/features/dashboard/components/NavMobile.tsx
+++ client-ecommerce/src/features/dashboard/components/NavMobile.tsx
@@ -14,7 +14,7 @@ const NavMobile: React.FC<NavMobileProps> = ({ isMenuOpen, setIsMenuOpen }) => {
   const menuToRender = user?.role === "ADMIN" ? [...navLinksMobile, ...navLinksMobileAdmin] : navLinksMobile;
   return (
     <div
-      className={`pt-20 fixed top-0 left-0 w-full h-screen overflow-y-auto bg-black/95 backdrop-blur-md border-r border-[#00f0ff]/30 text-base flex flex-col lg:hidden items-center justify-center gap-8 font-mono z-50 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
+      className={`pt-28 fixed top-0 left-0 w-full h-full overflow-y-auto bg-black/95 backdrop-blur-md border-r border-[#00f0ff]/30 text-base flex flex-col lg:hidden items-center justify-center gap-8 font-mono z-50 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
         }`}
     >
       <div className="absolute top-0 right-0 w-full h-1 bg-gradient-to-r from-transparent via-[#00f0ff] to-transparent opacity-50" />
@@ -26,14 +26,14 @@ const NavMobile: React.FC<NavMobileProps> = ({ isMenuOpen, setIsMenuOpen }) => {
             key={i}
             href={item.link}
             onClick={() => setIsMenuOpen(false)}
-            className="w-full text-center py-4 border-b border-zinc-800 text-zinc-400 font-bold uppercase tracking-widest hover:text-[#00f0ff] hover:border-[#00f0ff] hover:bg-[#00f0ff]/5 transition-all"
+            className="w-full text-xs text-center py-4 border-b border-zinc-800 text-zinc-400 font-bold uppercase tracking-widest hover:text-[#00f0ff] hover:border-[#00f0ff] hover:bg-[#00f0ff]/5 transition-all"
           >
             [{item.name}]
           </a>
         ))}
       </div>
 
-      <button className="bg-transparent border border-[#ff0055] text-[#ff0055] px-8 py-4 font-bold uppercase tracking-widest hover:bg-[#ff0055]/10 shadow-[0_0_10px_rgba(255,0,85,0.15)] transition-all">
+      <button className="bg-transparent border border-[#ff0055] text-[#ff0055] px-8 py-2 font-bold uppercase tracking-widest hover:bg-[#ff0055]/10 shadow-[0_0_10px_rgba(255,0,85,0.15)] transition-all">
         [TERMINATE_SESSION]
       </button>
 
